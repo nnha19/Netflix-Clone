d7fdaa7f9f09603bc41c9516925d2d9c235de732
Nyi Nyi Hmue Aung <webtekprogrammer@gmail.com>
2021-05-02 16:12:26 +0630

    makes some changes

--- client/src/homePage/pages/viewAllMovieCatePage.js
+++ client/src/homePage/pages/viewAllMovieCatePage.js
@@ -4,10 +4,10 @@ import { useParams } from "react-router-dom";
 
 const ViewAllMovieCatePage = () => {
   const { movieCategory } = useParams();
-  console.log(movieCategory);
+
   return (
     <>
-      <h3>{movieCategory}</h3>
+      <h3 className="primary-heading">{movieCategory}</h3>
     </>
   );
 };
