02155758cdbee3865b7bdbe655d8cbcf01412f40
WebTek828 <webtekprogrammer@gmail.com>
2021-05-05 13:38:15 +0630

    view detail has been displayed title and date

--- client/src/homePage/Components/ChildrenMovies/ViewDetail/ViewDetail.js
+++ client/src/homePage/Components/ChildrenMovies/ViewDetail/ViewDetail.js
@@ -6,17 +6,25 @@ import "./ViewDetail.css";
 
 const ViewDetail = (props) => {
   const movie = props.movie;
-  console.log(movie);
+  const releasedDate = movie.release_date.split("-")[0];
   return (
-    <div className="view-detail">
-      <img
-        className="view-detail__img"
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-      />
-      <div className="view-detail__body">
-        <IconWithTag detailPage={movie} />
+    <>
+      <div className="backdrop">
+        <div className="view-detail">
+          <img
+            className="view-detail__img"
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          />
+          <div className="view-detail__body">
+            <IconWithTag detailPage={movie} />
+            <h3 className="primary-heading view-detail__heading">
+              <span className="primary-heading">{movie.title}</span>
+              <span className="primary-heading">{releasedDate}</span>
+            </h3>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
